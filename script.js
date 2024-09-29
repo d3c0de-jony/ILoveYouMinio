10f30994c1905eef3ab049768dc61627f07a67b3
Dead SOul <tchsaan@gmail.com>
2024-09-29 12:50:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("hello world");
+
+
+// function for change the display text //
+function displayText() {
+    document.getElementById("display-text").innerHTML = "Hello MINIO";
+    document.getElementById("display-text").style.color = "red";
+
+}
